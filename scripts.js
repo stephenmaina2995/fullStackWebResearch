ae34fe8eb8954a08ed6fdbf896442f4df399f6ae
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-03-23 16:35:08 +0300

    Awesome revision

---
+++ scripts.js
@@ -0,0 +1,7 @@
+//console.log('hello world');
+//document.write('hello world');
+document.addEventListener("click", myFunction);
+// myFunction.addEventListener("")
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
